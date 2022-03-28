9551a32859774770aa0e6048f7f22a3d43b9ec95
Arty <8100818@gmail.com>
2022-03-28 12:43:32 +0300

    HW_Ex14: Формирование фреймворка

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,5 +1,6 @@
 import json.decoder
 from requests import Response
+from datetime import datetime
 
 class BaseCase:
     def get_cookie(self, response: Response, cookie_name):
@@ -18,3 +19,17 @@ class BaseCase:
 
         assert  name in resposne_as_dict, f"Response JSON doesn't have key '{name}'"
         return  resposne_as_dict[name]
+
+    def prepare_registartion_data(self, email=None):
+        if email is None:
+            base_part = "lernqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'lernqa',
+            'firstName': 'lernqa',
+            'lastName': 'lernqa',
+            'email': email
+        }
